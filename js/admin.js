4efed48df564108b16607d4379e321698d16592c
Thokozani Nqwili <simthokozile@gmail.com>
2022-12-08 17:25:35 +0200

    commit

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,5 @@
-
-let products = JSON.parse(localStorage.getItem(".records"))
-  ? JSON.parse(localStorage.getItem(".accomodation"))
+let accommodationlist = JSON.parse(localStorage.getItem("accomodationlist"))
+  ? JSON.parse(localStorage.getItem("accomodationlist"))
   : [
       {
         accomodationID: 1,
@@ -8,8 +7,6 @@ let products = JSON.parse(localStorage.getItem(".records"))
         imgUrl: "https://i.postimg.cc/Y060pkGY/lagoon.jpg",
         accomodationPrice: 1670,
         accomodationLocation: "Cape Town",
-        
-        
       },
       {
         accomodationID: 2,
@@ -17,17 +14,14 @@ let products = JSON.parse(localStorage.getItem(".records"))
         imgUrl: "https://i.postimg.cc/Qd2vMv8Y/glen-boutique-hotel.jpg",
         accomodationPrice: 1750,
         accomodationLocation: "Cape Town",
-        
-        
       },
       {
         accomodationID: 3,
         accomodationName: "The Maslow Hotel",
-        imgUrl: "https://i.postimg.cc/Gh4y1Nr2/revato-480812-12100737-644980.jpg",
+        imgUrl:
+          "https://i.postimg.cc/Gh4y1Nr2/revato-480812-12100737-644980.jpg",
         accomodationPrice: 1291,
         accomodationLocation: "Johanesburg",
-        
-        
       },
       {
         accomodationID: 4,
@@ -35,8 +29,6 @@ let products = JSON.parse(localStorage.getItem(".records"))
         imgUrl: "https://i.postimg.cc/FHLGhp3y/aquanzi.jpg",
         accomodationPrice: 1496,
         accomodationLocation: "Cape Town",
-        
-        
       },
       {
         accomodationID: 5,
@@ -44,114 +36,86 @@ let products = JSON.parse(localStorage.getItem(".records"))
         imgUrl: "https://i.postimg.cc/pTQwGxq6/anthurium-lodge-b-b.jpg",
         accomodationPrice: 1152,
         accomodationLocation: "Durban",
-        
-        
       },
       {
-        accomodationID:6,
+        accomodationID: 6,
         accomodationName: "The Riverside Hotel",
         imgUrl: "https://i.postimg.cc/Qd2vMv8Y/glen-boutique-hotel.jpg",
         accomodationPrice: 1384,
         accomodationLocation: "Cape Town",
-       
-        
       },
-    ]
-
-async function addRecords(){ 
-let record = document.querySelector(".records");
-  products.forEach((item) => {
+    ];
+function display() {
+  let record = document.querySelector(".records");
+  accommodationlist.forEach((item) => {
     record.innerHTML += `
-  <tbody>
-    <tr>
-      <th scope="row">${item.accomodationID}</th>
-      <td>${item.accomodationName}</td>
-      <td>${item.imgUrl}</td>
-      <td>R,${item.accomodationPrice}</td>
-      <td>${item.accomodationLocation}</td>
-      <td><i class='fas fa-edit' style='font-size:20px'></i>/<i class='fas fa-trash-alt' style='font-size:24px'></i></td>
-      <td></td>
-    </tr>
-    </tbody>`;
-    
-
-
-
-
-  // });
-});
+    <tbody>
+      <tr>
+        <th scope="row">${item.accomodationID}</th>
+        <td>${item.accomodationName}</td>
+        <td>${item.imgUrl}</td>
+        <td>R,${item.accomodationPrice}</td>
+        <td>${item.accomodationLocation}</td>
+        <td><i class='fas fa-edit' style='font-size:20px'></i></td>
+        <td><i class='fas fa-trash-alt' style='font-size:20px' onclick="deleteAccomodation(this)"></i></td>
+        <td></td>
+      </tr>
+      </tbody>`;
+  });
+  
+  
 }
-addRecords();
+display();
+
+const save = document.querySelector(".btnSave");
+const close = document.querySelector(".btnClose");
+const sortbyPrice = document.querySelector(".btnPrice");
+
+save.addEventListener("click", (e) => {
+  e.preventDefault();
+  let accomodationID = parseInt(
+    document.querySelector("#accomodationID").value
+  );
+  let accomodationName = document.querySelector("#accomodationName").value;
+  let imgUrl = document.querySelector("#imgUrl").value;
+  let accomodationPrice = parseInt(
+    document.querySelector("#accomodationPrice").value
+  );
+  let accomodationLocation = document.querySelector(
+    "#accomodationLocation"
+  ).value;
+
+  accommodationlist.push({
+    accomodationID,
+    accomodationName,
+    imgUrl,
+    accomodationPrice,
+    accomodationLocation,
+  });
+  localStorage.setItem("accomodationlist", JSON.stringify(accommodationlist));
+  console.table(accommodationlist);
+  display();
+});
+
+function deleteAccomodation(id) {
+  var i = id.parentNode.parentNode.rowIndex;
+  document.querySelector(".records").deleteRow(i);
+  accommodationlist.pop({
+    accomodationID,
+    accomodationName,
+    imgUrl,
+    accomodationPrice,
+    accomodationLocation,
+  });
+  localStorage.setItem("accomodationlist", JSON.stringify(accommodationlist));
+  console.table(accommodationlist);
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const btnAdd = document.querySelector(".btnAdd"); 
-// const btnSave = document.querySelector(".btnSave");
-// let accomodationArray=[];
-
-// function showForm(){
-//   btnAdd.addEventListener('click',(e)=>{
-//     e.preventDefault();
-  
-//     const form = document.querySelector(".adminForm");
-  
-//     if(form.style.visibility === 'hidden') {
-//       form.style.visibility = 'visible';
-//     } 
-//     else {
-//       form.style.visibility = 'hidden'
-//     };
-//   });
-
-// }
-// showForm()
-
-
-// async function addAccomodation(){
-
-// btnSave.addEventListener('click',(e)=>{
-//   e.preventDefault();
-//  let accomodationID = document.querySelector('#accomodationID').value;
-//  let accomodationName = document.querySelector('#accomodationName').value;
-//  let imgUrl = document.querySelector('#imgUrl').value;
-//  let accomodationPrice = (document.querySelector('#accomodationPrice').value);
-//  let accomodationLocation = document.querySelector('#accomodationLocation').value;
-
-//  // Push an object into an array 
-//  accomodationArray.push( 
-//   {
-//       accomodationID,
-//       accomodationName,
-//       imgUrl,
-//       accomodationPrice,
-//       accomodationLocation
+  function sort(){
+    accommodationlist.sort(
+      (p1, p2) =>
+      (p1.accomodationPrice < p2.accomodationPrice) ? 1 : (p1.accomodationPrice > p2.accomodationPrice) ? -1 : 0);
+      console.table(accommodationlist);
       
-//   }
-//  )
-//   console.table(accomodationArray);
-//     // Local Storage
-//     localStorage.setItem('data', JSON.stringify(accomodationArray));
-
-// location.reload();
-
-// });
-// }
-// addAccomodation();
-
-
-
-
-  
+  }
--- js/index.js
+++ js/index.js
@@ -1,67 +1,66 @@
-let accomodation = JSON.parse(localStorage.getItem(".accomodation"))
-  ? JSON.parse(localStorage.getItem(".accomodation"))
+let accomodation = JSON.parse(localStorage.getItem("accomodation"))
+  ? JSON.parse(localStorage.getItem("accomodation"))
   : [
-      {
-        id: 1,
-        name: "Lagoon Beach Hotel",
-        imgUrl: "https://i.postimg.cc/Y060pkGY/lagoon.jpg",
-        price: 1670,
-        coastal: true,
-        location: "Cape Town",
-        description:"Overlooking Table Mountain and Robben Island, Lagoon Beach Hotel offers direct beach access in Cape Town's Milnerton suburb.Equipped with a satellite TV, mini-bar, tea-and-coffee-making facilities and private bathrooms"
-        
-      },
-      {
-        id: 2,
-        name: "The Glen Boutique Hotel",
-        imgUrl: "https://i.postimg.cc/Qd2vMv8Y/glen-boutique-hotel.jpg",
-        price: 1750,
-        coastal: true,
-        location: "Cape Town",
-        description:"The Glen is close to everything that the Mother City offers, including beaches, dining, night-life.Rooms are comfortable with en-suite bathrooms, TV, WiFi."
-        
-      },
-      {
-        id: 3,
-        name: "The Maslow Hotel",
-        imgUrl: "https://i.postimg.cc/Gh4y1Nr2/revato-480812-12100737-644980.jpg",
-        price: 1291,
-        coastal: false,
-        location: "Johanesburg",
-        description:"Situated in the heart of Sandton's business district, home to top investment banks, financial institutions, the Gautrain station and the Johannesburg Stock Exchange."
-        
-      },
-      {
-        id: 4,
-        name: "Aquanzi Lodge",
-        imgUrl: "https://i.postimg.cc/FHLGhp3y/aquanzi.jpg",
-        price: 1496,
-        coastal: false,
-        location: "Cape Town",
-        description:"The 4 Star Aquanzi Lodge is situated just minutes from the upmarket Fourways suburb, set in a superb indigenous garden between aloes and acacia trees and offers comfortable and luxurious bed and breakfast and self-catering accommodation "
-        
-      },
-      {
-        id: 5,
-        name: "Anthurium Lodge B & B",
-        imgUrl: "https://i.postimg.cc/pTQwGxq6/anthurium-lodge-b-b.jpg",
-        price: 1152,
-        coastal: true,
-        location: "Durban",
-        description:"Our bedrooms are clean, spacious and very comfortable. We serve a buffet and cooked breakfast under our popular Gazebo, overlooking the koi pond and pool."
-        
-      },
-      {
-        id: 6,
-        name: "The Riverside Hotel",
-        imgUrl: "https://i.postimg.cc/Qd2vMv8Y/glen-boutique-hotel.jpg",
-        price: 1384,
-        coastal: true,
-        location: "Cape Town",
-        description:"Five minutes from the city centre of Durban, with wonderful views across a magnificent green belt stretching from the Umgeni River to the Indian Ocean. The Riverside Hotel reflects resort life in the city."
-        
-      },
-    ];
+  {
+    id: 1,
+    name: "Lagoon Beach Hotel",
+    imgUrl: "https://i.postimg.cc/Y060pkGY/lagoon.jpg",
+    price: 1670, 
+    location: "Cape Town",
+    description:
+      "Overlooking Table Mountain and Robben Island, Lagoon Beach Hotel offers direct beach access in Cape Town's Milnerton suburb.Equipped with a satellite TV, mini-bar, tea-and-coffee-making facilities and private bathrooms",
+  },
+  {
+    id: 2,
+    name: "The Glen Boutique Hotel",
+    imgUrl: "https://i.postimg.cc/Qd2vMv8Y/glen-boutique-hotel.jpg",
+    price: 1750,
+   
+    location: "Cape Town",
+    description:
+      "The Glen is close to everything that the Mother City offers, including beaches, dining, night-life.Rooms are comfortable with en-suite bathrooms, TV, WiFi.",
+  },
+  {
+    id: 3,
+    name: "The Maslow Hotel",
+    imgUrl: "https://i.postimg.cc/Gh4y1Nr2/revato-480812-12100737-644980.jpg",
+    price: 1291,
+    
+    location: "Johanesburg",
+    description:
+      "Situated in the heart of Sandton's business district, home to top investment banks, financial institutions, the Gautrain station and the Johannesburg Stock Exchange.",
+  },
+  {
+    id: 4,
+    name: "Aquanzi Lodge",
+    imgUrl: "https://i.postimg.cc/FHLGhp3y/aquanzi.jpg",
+    price: 1496,
+   
+    location: "Johanesburg",
+    description:
+      "The 4 Star Aquanzi Lodge is situated just minutes from the upmarket Fourways suburb, set in a superb indigenous garden between aloes and acacia trees and offers comfortable and luxurious bed and breakfast and self-catering accommodation ",
+  },
+  {
+    id: 5,
+    name: "Anthurium Lodge B & B",
+    imgUrl: "https://i.postimg.cc/pTQwGxq6/anthurium-lodge-b-b.jpg",
+    price: 1152,
+    coastal: true,
+    location: "Durban",
+    description:
+      "Our bedrooms are clean, spacious and very comfortable. We serve a buffet and cooked breakfast under our popular Gazebo, overlooking the koi pond and pool.",
+  },
+  {
+    id: 6,
+    name: "The Riverside Hotel",
+    imgUrl: "https://i.postimg.cc/Qd2vMv8Y/glen-boutique-hotel.jpg",
+    price: 1384,
+    coastal: true,
+    location: "Durban",
+    description:
+      "Five minutes from the city centre of Durban, with wonderful views across a magnificent green belt stretching from the Umgeni River to the Indian Ocean. The Riverside Hotel reflects resort life in the city.",
+  },
+];
 
 async function display() {
   let display = document.querySelector(".accomodation");
@@ -78,6 +77,24 @@ async function display() {
   });
 }
 display();
+
+let filterlocation = document.getElementById("location");
+filterlocation.addEventListener("change", (e) => {
+var newlocation = accomodation.filter(item => {
+  return item.location == e.t
+  arget.value
+}
+);
+console.log(newlocation);
+  display();
+   });
+
+
+
+
+
+
+
 
 
 
